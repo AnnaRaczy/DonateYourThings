c5c2eede293fa7f63986e34bac5f71c0f1378f0b
AnnaRaczy <38297862+AnnaRaczy@users.noreply.github.com>
2022-01-25 22:17:33 +0100

    Home components

--- src/components/Home/HomeContact.jsx
+++ src/components/Home/HomeContact.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 
+const FormButton = () => {
+  return <button>Send</button>;
+};
+
+const Form = () => {
+  return (
+    <form>
+      <label></label>
+      <input type="text" />
+      <label></label>
+      <input type="text" />
+      <label></label>
+      <input type="text" />
+    </form>
+  );
+};
+
+const Title = () => {
+  return <h1>Contact us</h1>;
+};
+
 const HomeContact = () => {
   return (
     <div>
-      <div class="photo"></div>
-      <div>
-        <h1>Contact us</h1>
-        <form>
-          <label></label>
-          <input type="text" />
-          <label></label>
-          <input type="text" />
-          <label></label>
-          <input type="text" />
-        </form>
-        <button>Send</button>
-      </div>
+      <Title />
+      <Form />
+      <FormButton />
     </div>
   );
 };
